52dc200ba1f0c2cefff68634b71d6f326e4a6e10
Roshan Kenia <keniaroshan@gmail.com>
2022-12-05 21:28:50 -0500

    Update simpleCNN.py

--- simpleCNN.py
+++ simpleCNN.py
@@ -1,5 +1,15 @@
 import torch.nn as nn
 import torch.nn.functional as F
+# ensure we are running on the correct gpu
+import os
+import sys
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "6"  # (xxxx is your specific GPU ID)
+if not torch.cuda.is_available() or torch.cuda.device_count() != 1:
+    print('exiting')
+    sys.exit()
+else:
+    print('GPU is being properly used')
 
 
 class Net(nn.Module):
