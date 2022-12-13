2bc6591961a343601152a73b6b464678687c042f
Roshan Kenia <keniaroshan@gmail.com>
2022-12-13 09:38:57 -0500

    Update detection_video.py

--- detection_video.py
+++ detection_video.py
@@ -42,77 +42,28 @@ def normalize(arr):
 
 
 # define our own dataset
-class PebbleDataset(torch.utils.data.Dataset):
+class PebbleTestDataset(torch.utils.data.Dataset):
     def __init__(self, root, transforms=None):
         self.root = root
         self.transforms = transforms
         # load all image and annotation files, sorting them to ensure that the images and their annotations are aligned
-        self.imgs = list(sorted(os.listdir(os.path.join(root, "JPEGImages"))))
-        self.bbox_xml = list(
-            sorted(os.listdir(os.path.join(root, "Annotations"))))
+        self.imgs = list(sorted(os.listdir(root)))
 
     def __getitem__(self, idx):
         # load images and their corresponding bbox annotations
         img_path = os.path.join(self.root, "JPEGImages", self.imgs[idx])
-        bbox_xml_path = os.path.join(
-            self.root, "Annotations", self.bbox_xml[idx])
         img = Image.open(img_path).convert("RGB")
         # normalization
         #img = np.array(img)
         #img = normalize(img)
         #img = Image.fromarray(normalize(img),'RGB')
 
-        # read the annotation files from the path, which are in xml format
-        dom = parse(bbox_xml_path)
-        # get the element of the annotation files
-        data = dom.documentElement
-        # get the objects of the elements
-        objects = data.getElementsByTagName('object')
-        # extract the content of the annotation file, which includes class label and bounding box coordinates
-        boxes = []
-        labels = []
-        for object_ in objects:
-            # extract the class label, 0 for background, 1 and 2 for mark_type_1 and mark_type_2 respectively
-            name = object_.getElementsByTagName(
-                'name')[0].childNodes[0].nodeValue
-            # labels.append(np.int(name[-1]))
-            labels.append(np.int(name.split("_")[-1]))
-
-            # extract the bounding box coordinates
-            bndbox = object_.getElementsByTagName('bndbox')[0]
-            xmin = np.float(bndbox.getElementsByTagName(
-                'xmin')[0].childNodes[0].nodeValue)
-            ymin = np.float(bndbox.getElementsByTagName(
-                'ymin')[0].childNodes[0].nodeValue)
-            xmax = np.float(bndbox.getElementsByTagName(
-                'xmax')[0].childNodes[0].nodeValue)
-            ymax = np.float(bndbox.getElementsByTagName(
-                'ymax')[0].childNodes[0].nodeValue)
-            boxes.append([xmin, ymin, xmax, ymax])
-
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        # there is only one class
-        labels = torch.as_tensor(labels, dtype=torch.int64)
-
-        image_id = torch.tensor([idx])
-        # calculate the area of the bounding box of each instance
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        # suppose all instances are not crowd
-        iscrowd = torch.zeros((len(objects),), dtype=torch.int64)
-
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
-        target["image_id"] = image_id
-        target["area"] = area
-        target["iscrowd"] = iscrowd
-
         if self.transforms is not None:
             # https://github.com/pytorch/vision/tree/master/references/detection
             # On this website, there are transform examples of RandomHorizontalFlip for target in transforms.py
-            img, target = self.transforms(img, target)
+            img = self.transforms(img)
 
-        return img, target
+        return img
 
     def __len__(self):
         return len(self.imgs)
@@ -134,8 +85,7 @@ def get_transform(train):
 # Start to train the model
 ###############################
 # change the root path depending on your own dataset path
-root = r'new_datasets_annotations/doe_dataset_GT_white_ink_voc_20220901_600'
-root1 = r'new_datasets_annotations/doe_dataset_GT_ceramic_painted_voc_20220901_600'
+root1 = r'./ceramicimages'
 
 # train on the GPU (specify GPU ID with 'cuda:id'), or on the CPU if a GPU is not available
 device = torch.device(
@@ -146,9 +96,7 @@ num_classes = 11
 # note that the 'dataset' and 'dataset_test' are with the same images in the same order
 # the only difference is that images in 'dataset' can be randomly flipped,
 # while images in 'dataset_test' are not flipped
-dataset = PebbleDataset(root, get_transform(train=True))
-dataset_test = PebbleDataset(root1, get_transform(train=False))
-print("number of images in the training set:", len(dataset))
+dataset_test = PebbleTestDataset(root1, get_transform(train=False))
 print("number of images in the testing set:", len(dataset_test))
 
 """
@@ -159,57 +107,6 @@ indices = torch.randperm(len(dataset)).tolist()
 dataset = torch.utils.data.Subset(dataset, indices[:s_ratio])
 dataset_test = torch.utils.data.Subset(dataset_test, indices[s_ratio:])
 """
-
-# define training and validation data loaders
-data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=2, shuffle=True,  # num_workers=4,
-    collate_fn=utils.collate_fn)
-
-data_loader_test = torch.utils.data.DataLoader(
-    dataset_test, batch_size=2, shuffle=False,  # num_workers=4,
-    collate_fn=utils.collate_fn)
-
-# get the model using our helper function
-model = models.detection.fasterrcnn_resnet50_fpn(
-    pretrained=False, progress=True, num_classes=num_classes, pretrained_backbone=True)  # or get_object_detection_model(num_classes)
-model = models.detection.fasterrcnn_resnet50_fpn_v2(
-    pretrained=False, progress=True, num_classes=num_classes, pretrained_backbone=True)  # or get_object_detection_model(num_classes)
-
-# move model to the right device
-model.to(device)
-
-# collect the trainable parameters in the model
-params = [p for p in model.parameters() if p.requires_grad]
-
-# define the optimizer, here we use SGD
-optimizer = torch.optim.SGD(
-    params, lr=0.0003, momentum=0.9, weight_decay=0.0005)
-
-# define a learning rate scheduler
-lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
-    optimizer, T_0=1, T_mult=2)
-
-# let's train it for a defined number of epochs
-num_epochs = 200
-
-for epoch in range(num_epochs):
-    # train for one epoch, printing every 10 iterations
-    # images and targets are all put '.to(device)' by the 'train_one_epoch' in engine.py
-    train_one_epoch(model, optimizer, data_loader,
-                    device, epoch, print_freq=50)
-    # update the learning rate
-    lr_scheduler.step()
-    # evaluate on the test dataset
-    evaluate(model, data_loader_test, device=device)
-
-    print('')
-    print('==================================================')
-    print('')
-print("Training is done!")
-
-
-# save the trained model
-torch.save(model, r'./saved_model/model_doe_ceramic_paint_fastRCNN_v2_200epoch.pkl')
 
 
 def fig_draw(img, prediction, idx):
@@ -311,7 +208,7 @@ def showbbox(model, img, idx):
     plt.figure(figsize=(50,50))
     plt.imshow(img)
     plt.axis('off')
-    vis_tgt_path = "./visualization_results/ceramic_paint_visual_samples_fastRCNN_v2_200epochs/"
+    vis_tgt_path = "./visualization_results/videoceramic/"
     if not os.path.isdir(vis_tgt_path):
         os.mkdir(vis_tgt_path)
     plt.savefig(os.path.join(vis_tgt_path, "sample_" + str(idx) + "_vis.png"))
@@ -324,5 +221,5 @@ model = torch.load(
 model.to(device)
 
 for idx in range(len(dataset_test)):
-    img, _ = dataset_test[idx]
+    img = dataset_test[idx]
     showbbox(model, img, idx)
