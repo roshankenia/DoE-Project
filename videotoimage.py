c52c5bce0c20f20198a9f5b8414e8800abbdbacd
Roshan Kenia <keniaroshan@gmail.com>
2022-12-20 23:58:15 -0500

    vidtoimage

--- videotoimage.py
+++ videotoimage.py
@@ -1,17 +1,29 @@
 import cv2
-vidcap = cv2.VideoCapture('Moving Pebbles - Ink Paint.MOV')
-def getFrame(sec):
-    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
-    hasFrames,image = vidcap.read()
+import numpy as np
+vidcap = cv2.VideoCapture('Moving Pebbles - Ceramic Paint.MOV')
+frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+print('video has', str(frame_count), 'frames.')
+
+rotations = [0, 45, 90, 135, 180, 225, 270, 315]
+# sharpen_kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+
+count = 0
+while (vidcap.isOpened()):
+    hasFrames, image = vidcap.read()
     if hasFrames:
-        cv2.imwrite("./inkimages/image"+str(count)+".jpg", image)     # save frame as JPG file
-    return hasFrames
-sec = 0
-frameRate = 0.1 #//it will capture image in each 0.5 second
-count=1
-success = getFrame(sec)
-while success:
-    count = count + 1
-    sec = sec + frameRate
-    sec = round(sec, 2)
-    success = getFrame(sec)
+        for rotation in rotations:
+            image_center = tuple(np.array(image.shape[1::-1]) / 2)
+            rot_mat = cv2.getRotationMatrix2D(image_center, rotation, 1.0)
+            result = cv2.warpAffine(
+                image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+            # result = cv2.filter2D(result, -1, sharpen_kernel)
+            # save frame as JPG file
+            cv2.imwrite("./ceramicimages/image"+str(count) +
+                        "_"+str(rotation)+".jpg", result)
+        count += 1
+    else:
+        break
+
+# When everything done, release the capture
+vidcap.release()
+cv2.destroyAllWindows()
