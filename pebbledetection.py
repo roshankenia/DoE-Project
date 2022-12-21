39316412d123db8782363781e57b24ef0fcb6d6d
Roshan Kenia <keniaroshan@gmail.com>
2022-12-21 11:31:16 -0500

    Update pebbledetection.py

--- pebbledetection.py
+++ pebbledetection.py
@@ -103,4 +103,4 @@ def object_detection_api(img_path, threshold=0.5, rect_th=3, text_size=3, text_t
     plt.close()
 
 
-object_detection_api('./bestceramic/image183_90.jpg')
+object_detection_api('./bestceramic/image183_90.jpg', threshold=0.8)
