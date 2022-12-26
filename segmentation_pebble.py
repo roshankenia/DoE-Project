cfc7bc14c702e6b852f9a3417192c77bbcf2b89d
Roshan Kenia <keniaroshan@gmail.com>
2022-12-25 23:20:10 -0500

    Update segmentation_pebble.py

--- segmentation_pebble.py
+++ segmentation_pebble.py
@@ -182,7 +182,7 @@ dataset_test = SegmentationDataset('./', get_transform(train=False))
 # split the dataset in train and test set
 torch.manual_seed(1)
 indices = torch.randperm(len(dataset)).tolist()
-num_train = int(0.8 * indices)
+num_train = int(0.8 * len(indices))
 dataset = torch.utils.data.Subset(dataset, indices[:num_train])
 dataset_test = torch.utils.data.Subset(dataset_test, indices[num_train:])
 
