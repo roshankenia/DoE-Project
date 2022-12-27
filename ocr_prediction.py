3f3a9ca59a77af4251f647278326cd7f6d1b2ffb
Roshan Kenia <keniaroshan@gmail.com>
2022-12-26 22:23:32 -0500

    OCR add to environment

--- ocr_prediction.py
+++ ocr_prediction.py
@@ -9,6 +9,8 @@ import torchvision.transforms as T
 import torch
 import matplotlib.pyplot as plt
 from PIL import Image
+from doctr.io import DocumentFile
+from doctr.models import ocr_predictor
 
 # ensure we are running on the correct gpu
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -19,14 +21,23 @@ if not torch.cuda.is_available() or torch.cuda.device_count() != 1:
 else:
     print('GPU is being properly used')
 
+
+model = ocr_predictor(pretrained=True)
+device = torch.device(
+    'cuda') if torch.cuda.is_available() else torch.device('cpu')
+model.to(device)
+
 root = "./ceramicimages"
 # need to iterate through each image folder
 imageFolders = list(sorted(os.listdir(root)))
 
-print(imageFolders)
-
 for imageFolder in imageFolders:
     # obtain all image rotations
     rotations = list(sorted(os.listdir(os.path.join(root, imageFolder))))
-    print(rotations)
-    break
+    rotationsWithRoot = []
+    for rotation in rotations:
+        rotationsWithRoot.append(os.path.join(root, imageFolder, rotation))
+    # create document image
+    all_rotations_doc = DocumentFile.from_images(rotationsWithRoot)
+
+    result = model(all_rotations_doc)
