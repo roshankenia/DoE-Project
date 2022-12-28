017cb3afe332070d929c1956d67dd12d086eb55e
Roshan Kenia <keniaroshan@gmail.com>
2022-12-27 22:47:45 -0500

    Update rotData.py

--- rotatedDataTrial/rotData.py
+++ rotatedDataTrial/rotData.py
@@ -11,6 +11,8 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from xml.dom.minidom import parse
 import math
+import shutil
+
 
 # ensure we are running on the correct gpu
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
@@ -20,102 +22,6 @@ if not torch.cuda.is_available() or torch.cuda.device_count() != 1:
     sys.exit()
 else:
     print('GPU is being properly used')
-
-
-def rotate(origin, point, angle):
-    """
-    Rotate a point counterclockwise by a given angle around a given origin.
-
-    The angle should be given in radians.
-    """
-    ox, oy = origin
-    px, py = point
-
-    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
-    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
-    return qx, qy
-
-# make image using box
-
-
-def make_image(img, boxes, labels, img_num, rotation, rect_th=2, text_size=0.5, text_th=1):
-    for i in range(len(boxes)):
-        cv2.rectangle(img, (int(boxes[i][0]), int(boxes[i][1])), (int(boxes[i][2]), int(boxes[i][3])),
-                      color=(0, 255, 0), thickness=rect_th)
-        cv2.putText(img, str(labels[i]), (int(boxes[i][0]), int(boxes[i][1])), cv2.FONT_HERSHEY_SIMPLEX,
-                    text_size, (0, 255, 0), thickness=text_th)
-    # save frame as JPG file
-    cv2.imwrite("img_"+str(img_num)+"_"+str(rotation)+".jpg", img)
-
-
-# obtain data
-img = Image.open("./img_1053_og.jpg").convert("RGB")
-
-# read the annotation files from the path, which are in xml format
-dom = parse("./img_1053_x.xml")
-# get the element of the annotation files
-data = dom.documentElement
-# get the objects of the elements
-objects = data.getElementsByTagName('object')
-# extract the content of the annotation file, which includes class label and bounding box coordinates
-boxes = []
-labels = []
-for object_ in objects:
-    # extract the class label, 0 for background, 1 and 2 for mark_type_1 and mark_type_2 respectively
-    name = object_.getElementsByTagName(
-        'name')[0].childNodes[0].nodeValue
-    # labels.append(np.int(name[-1]))
-    labels.append(np.int(name.split("_")[-1]))
-
-    # extract the bounding box coordinates
-    bndbox = object_.getElementsByTagName('bndbox')[0]
-    xmin = np.float64(bndbox.getElementsByTagName(
-        'xmin')[0].childNodes[0].nodeValue)
-    ymin = np.float64(bndbox.getElementsByTagName(
-        'ymin')[0].childNodes[0].nodeValue)
-    xmax = np.float64(bndbox.getElementsByTagName(
-        'xmax')[0].childNodes[0].nodeValue)
-    ymax = np.float64(bndbox.getElementsByTagName(
-        'ymax')[0].childNodes[0].nodeValue)
-    boxes.append([xmin, ymin, xmax, ymax])
-
-print('boxes:', boxes)
-print('labels:', labels)
-img_num = rot = ''.join(filter(lambda i: i.isdigit(), "img_1053_og.jpg"))
-rotations = [0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165,
-             180, 195, 210, 225, 240, 255, 270, 285, 300, 315, 330, 345]
-# for rotation in rotations:
-#     # convert to cv2 image
-#     image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-#     h, w = image.shape[:2]
-#     cx, cy = (int(w / 2), int(h / 2))
-#     # get center
-#     image_center = tuple(np.array(image.shape[1::-1]) / 2)
-#     rot_mat = cv2.getRotationMatrix2D(
-#         image_center, rotation, 1.0)
-#     # rotate image
-#     result = cv2.warpAffine(
-#         image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
-#     # rotate points
-#     rotatedBoxes = []
-#     for box in boxes:
-#         cos, sin = abs(rot_mat[0, 0]), abs(rot_mat[0, 1])
-#         newW = int((h * sin) + (w * cos))
-#         newH = int((h * cos) + (w * sin))
-#         rot_mat[0, 2] += (newW / 2) - cx
-#         rot_mat[1, 2] += (newH / 2) - cy
-#         v = [box[0], box[1], 1]
-#         adjusted_coord_min = np.dot(rot_mat, v)
-#         v = [box[2], box[3], 1]
-#         adjusted_coord_max = np.dot(rot_mat, v)
-#         rotatedBoxes.append([int(adjusted_coord_min[0]), int(adjusted_coord_min[1]),
-#                             int(adjusted_coord_max[0]), int(adjusted_coord_max[1])])
-#         # xmin, ymin = rotate(image_center, (box[0], box[1]), rotation)
-#         # xmax, ymax = rotate(image_center, (box[2], box[3]), rotation)
-#         # rotatedBoxes.append([int(xmin), int(ymin), int(xmax), int(ymax)])
-#     # create rotation matrix
-#     # save frame as JPG file
-#     make_image(result, rotatedBoxes, labels, img_num, rotation)
 
 
 def rotate_im(image, angle):
@@ -294,6 +200,56 @@ def get_enclosing_box(corners):
 
     return final
 
+# make image using box
+
+
+def make_image(img, boxes, labels, img_num, rotation, rect_th=2, text_size=0.5, text_th=1):
+    for i in range(len(boxes)):
+        cv2.rectangle(img, (int(boxes[i][0]), int(boxes[i][1])), (int(boxes[i][2]), int(boxes[i][3])),
+                      color=(0, 255, 0), thickness=rect_th)
+        cv2.putText(img, str(labels[i]), (int(boxes[i][0]), int(boxes[i][1])), cv2.FONT_HERSHEY_SIMPLEX,
+                    text_size, (0, 255, 0), thickness=text_th)
+    # save frame as JPG file
+    cv2.imwrite("img_"+str(img_num)+"_"+str(rotation)+".jpg", img)
+
+
+# obtain data
+img = Image.open("./img_1053_og.jpg").convert("RGB")
+
+# read the annotation files from the path, which are in xml format
+dom = parse("./img_1053_x.xml")
+# get the element of the annotation files
+data = dom.documentElement
+# get the objects of the elements
+objects = data.getElementsByTagName('object')
+# extract the content of the annotation file, which includes class label and bounding box coordinates
+boxes = []
+labels = []
+for object_ in objects:
+    # extract the class label, 0 for background, 1 and 2 for mark_type_1 and mark_type_2 respectively
+    name = object_.getElementsByTagName(
+        'name')[0].childNodes[0].nodeValue
+    # labels.append(np.int(name[-1]))
+    labels.append(np.int(name.split("_")[-1]))
+
+    # extract the bounding box coordinates
+    bndbox = object_.getElementsByTagName('bndbox')[0]
+    xmin = np.float64(bndbox.getElementsByTagName(
+        'xmin')[0].childNodes[0].nodeValue)
+    ymin = np.float64(bndbox.getElementsByTagName(
+        'ymin')[0].childNodes[0].nodeValue)
+    xmax = np.float64(bndbox.getElementsByTagName(
+        'xmax')[0].childNodes[0].nodeValue)
+    ymax = np.float64(bndbox.getElementsByTagName(
+        'ymax')[0].childNodes[0].nodeValue)
+    boxes.append([xmin, ymin, xmax, ymax])
+
+print('boxes:', boxes)
+print('labels:', labels)
+img_num = rot = ''.join(filter(lambda i: i.isdigit(), "img_1053_og.jpg"))
+rotations = [0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165,
+             180, 195, 210, 225, 240, 255, 270, 285, 300, 315, 330, 345]
+
 
 img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
 for rotation in rotations:
@@ -324,3 +280,6 @@ for rotation in rotations:
     print(bboxes)
     # bboxes = clip_box(bboxes, [0, 0, w, h], 0.25)
     make_image(image, bboxes, labels, img_num, rotation)
+
+    # create copy xml
+    shutil.copy2("./img_1053_x.xml", "./img_1053_"+str(rotation)+".xml")
