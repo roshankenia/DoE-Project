f40258cb26dc77eaac2f589a220d22e32d328c77
Roshan Kenia <keniaroshan@gmail.com>
2022-12-28 12:00:00 -0500

    Update rotData.py

--- rotatedDataTrial/rotData.py
+++ rotatedDataTrial/rotData.py
@@ -204,110 +204,139 @@ def get_enclosing_box(corners):
 # make image using box
 
 
-def make_image(img, boxes, labels, img_num, rotation, rect_th=2, text_size=0.5, text_th=1):
+def make_image(img, boxes, labels, img_num, rotation, annotationVisPath, rect_th=2, text_size=0.5, text_th=1):
     for i in range(len(boxes)):
         cv2.rectangle(img, (int(boxes[i][0]), int(boxes[i][1])), (int(boxes[i][2]), int(boxes[i][3])),
                       color=(0, 255, 0), thickness=rect_th)
         cv2.putText(img, str(labels[i]), (int(boxes[i][0]), int(boxes[i][1])), cv2.FONT_HERSHEY_SIMPLEX,
                     text_size, (0, 255, 0), thickness=text_th)
     # save frame as JPG file
-    cv2.imwrite("./AnnotationsVisualization/img_"+str(img_num) +
-                "_"+str(rotation)+"_annotated.jpg", img)
+    cv2.imwrite(os.path.join(annotationVisPath, "img_" +
+                str(img_num) + "_"+str(rotation)+"_annotated.jpg"), img)
 
 
-# obtain data
-img = Image.open("./img_1053_og.jpg").convert("RGB")
+# make directories
+root = "./RotatedData/"
+if not os.path.isdir(root):
+    os.mkdir(root)
+annotationPath = "./RotatedData/Annotations/"
+if not os.path.isdir(annotationPath):
+    os.mkdir(annotationPath)
+annotationVisPath = "./RotatedData/AnnotationsVisualization/"
+if not os.path.isdir(annotationVisPath):
+    os.mkdir(annotationVisPath)
+jpegpath = "./RotatedData/JPEGImages/"
+if not os.path.isdir(jpegpath):
+    os.mkdir(jpegpath)
 
-# read the annotation files from the path, which are in xml format
-dom = parse("./img_1053_x.xml")
-# get the element of the annotation files
-data = dom.documentElement
-# get the objects of the elements
-objects = data.getElementsByTagName('object')
-# extract the content of the annotation file, which includes class label and bounding box coordinates
-boxes = []
-labels = []
-for object_ in objects:
-    # extract the class label, 0 for background, 1 and 2 for mark_type_1 and mark_type_2 respectively
-    name = object_.getElementsByTagName(
-        'name')[0].childNodes[0].nodeValue
-    # labels.append(np.int(name[-1]))
-    labels.append(np.int(name.split("_")[-1]))
+sourceRoot = "./new_datasets_annotations/doe_dataset_GT_ceramic_painted_voc_20220901_600/"
+sourceJPEGPath = os.path.join(sourceRoot, "JPEGImages")
+sourceAnnotationPath = os.path.join(sourceRoot, "Annotations")
 
-    # extract the bounding box coordinates
-    bndbox = object_.getElementsByTagName('bndbox')[0]
-    xmin = np.float64(bndbox.getElementsByTagName(
-        'xmin')[0].childNodes[0].nodeValue)
-    ymin = np.float64(bndbox.getElementsByTagName(
-        'ymin')[0].childNodes[0].nodeValue)
-    xmax = np.float64(bndbox.getElementsByTagName(
-        'xmax')[0].childNodes[0].nodeValue)
-    ymax = np.float64(bndbox.getElementsByTagName(
-        'ymax')[0].childNodes[0].nodeValue)
-    boxes.append([xmin, ymin, xmax, ymax])
+# obtain all JPEG image file names
+allJPEGImgs = list(sorted(os.listdir(sourceJPEGPath)))
+x = 0
+for imageFile in allJPEGImgs:
+    # obtain data
+    imagePIL = Image.open(os.path.join(
+        sourceJPEGPath, imageFile)).convert("RGB")
+    img_num = ''.join(filter(lambda i: i.isdigit(), imageFile))
 
-print('boxes:', boxes)
-print('labels:', labels)
-img_num = rot = ''.join(filter(lambda i: i.isdigit(), "img_1053_og.jpg"))
-rotations = [0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165,
-             180, 195, 210, 225, 240, 255, 270, 285, 300, 315, 330, 345]
+    # read the annotation files from the path, which are in xml format
+    dom = parse(os.path.join(sourceAnnotationPath, "img_"+str(img_num)+".xml"))
+    # get the element of the annotation files
+    data = dom.documentElement
+    # get the objects of the elements
+    objects = data.getElementsByTagName('object')
+    # extract the content of the annotation file, which includes class label and bounding box coordinates
+    boxes = []
+    labels = []
+    for object_ in objects:
+        # extract the class label, 0 for background, 1 and 2 for mark_type_1 and mark_type_2 respectively
+        name = object_.getElementsByTagName(
+            'name')[0].childNodes[0].nodeValue
+        # labels.append(np.int(name[-1]))
+        labels.append(np.int(name.split("_")[-1]))
 
+        # extract the bounding box coordinates
+        bndbox = object_.getElementsByTagName('bndbox')[0]
+        xmin = np.float64(bndbox.getElementsByTagName(
+            'xmin')[0].childNodes[0].nodeValue)
+        ymin = np.float64(bndbox.getElementsByTagName(
+            'ymin')[0].childNodes[0].nodeValue)
+        xmax = np.float64(bndbox.getElementsByTagName(
+            'xmax')[0].childNodes[0].nodeValue)
+        ymax = np.float64(bndbox.getElementsByTagName(
+            'ymax')[0].childNodes[0].nodeValue)
+        boxes.append([xmin, ymin, xmax, ymax])
 
-img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-for rotation in rotations:
-    print('\nRotation:', rotation)
-    image = np.copy(img)
-    w, h = image.shape[1], image.shape[0]
-    cx, cy = w//2, h//2
+    print('boxes:', boxes)
+    print('labels:', labels)
+    rotations = [0, 15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165,
+                 180, 195, 210, 225, 240, 255, 270, 285, 300, 315, 330, 345]
 
-    image = rotate_im(image, rotation)
-    bboxes = np.array(boxes)
-    corners = get_corners(bboxes)
+    img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    for rotation in rotations:
+        print('\nRotation:', rotation)
+        image = np.copy(img)
+        w, h = image.shape[1], image.shape[0]
+        cx, cy = w//2, h//2
 
-    corners = np.hstack((corners, bboxes[:, 4:]))
+        image = rotate_im(image, rotation)
+        bboxes = np.array(boxes)
+        corners = get_corners(bboxes)
 
-    corners[:, :8] = rotate_box(corners[:, :8], rotation, cx, cy, h, w)
+        corners = np.hstack((corners, bboxes[:, 4:]))
 
-    new_bbox = get_enclosing_box(corners)
+        corners[:, :8] = rotate_box(corners[:, :8], rotation, cx, cy, h, w)
 
-    scale_factor_x = image.shape[1] / w
+        new_bbox = get_enclosing_box(corners)
 
-    scale_factor_y = image.shape[0] / h
+        scale_factor_x = image.shape[1] / w
 
-    image = cv2.resize(image, (w, h))
+        scale_factor_y = image.shape[0] / h
 
-    new_bbox[:, :4] /= [scale_factor_x,
-                        scale_factor_y, scale_factor_x, scale_factor_y]
+        image = cv2.resize(image, (w, h))
 
-    bboxes = new_bbox
-    print(bboxes)
-    # save frame as JPG file
-    cv2.imwrite("./JPEGImages/img_"+str(img_num) +
-                "_"+str(rotation)+".jpg", image)
-    # bboxes = clip_box(bboxes, [0, 0, w, h], 0.25)
-    make_image(image, bboxes, labels, img_num, rotation)
+        new_bbox[:, :4] /= [scale_factor_x,
+                            scale_factor_y, scale_factor_x, scale_factor_y]
 
-    # create copy xml
-    shutil.copy2("./img_1053_x.xml",
-                 "./Annotations/img_1053_"+str(rotation)+".xml")
+        bboxes = new_bbox
+        print(bboxes)
+        # save frame as JPG file
 
-    tree = ElementTree()
-    tree.parse("./Annotations/img_1053_"+str(rotation)+".xml")
+        cv2.imwrite(os.path.join(jpegpath, "img_" +
+                    str(img_num) + "_"+str(rotation)+".jpg"), image)
+        # bboxes = clip_box(bboxes, [0, 0, w, h], 0.25)
+        make_image(image, bboxes, labels, img_num, rotation)
 
-    filename = tree.find('filename')
-    filename.text = "img_1053_"+str(rotation)+".jpg"
+        # create copy xml
 
-    objects = tree.findall('object')
-    for o in range(len(objects)):
-        object = objects[o]
-        bbox = bboxes[o]
-        bndbox = object.find('bndbox')
-        xmin, ymin, xmax, ymax = list(bndbox)
-        print('Old:', xmin.text, ymin.text, xmax.text, ymax.text)
-        print('New:', bbox[0], bbox[1], bbox[2], bbox[3])
-        xmin.text = str(bbox[0])
-        ymin.text = str(bbox[1])
-        xmax.text = str(bbox[2])
-        ymax.text = str(bbox[3])
+        shutil.copy2(os.path.join(sourceAnnotationPath, "img_"+str(img_num) + ".xml"),
+                     os.path.join(annotationPath, "img_" + str(img_num) + "_"+str(rotation)+".xml"))
 
-    tree.write("./Annotations/img_1053_"+str(rotation)+".xml")
+        tree = ElementTree()
+        tree.parse(os.path.join(annotationPath, "img_" +
+                   str(img_num) + "_"+str(rotation)+".xml"))
+
+        filename = tree.find('filename')
+        filename.text = "img_" + str(img_num) + "_"+str(rotation)+".jpg"
+
+        objects = tree.findall('object')
+        for o in range(len(objects)):
+            object = objects[o]
+            bbox = bboxes[o]
+            bndbox = object.find('bndbox')
+            xmin, ymin, xmax, ymax = list(bndbox)
+            print('Old:', xmin.text, ymin.text, xmax.text, ymax.text)
+            print('New:', bbox[0], bbox[1], bbox[2], bbox[3])
+            xmin.text = str(bbox[0])
+            ymin.text = str(bbox[1])
+            xmax.text = str(bbox[2])
+            ymax.text = str(bbox[3])
+
+        tree.write(os.path.join(annotationPath, "img_" +
+                   str(img_num) + "_"+str(rotation)+".xml"))
+        x += 1
+        if x == 5:
+            break
