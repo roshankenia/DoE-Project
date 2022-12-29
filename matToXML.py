ed34712af83a7632fc88fcc336eb4081c0a0e106
Roshan Kenia <keniaroshan@gmail.com>
2022-12-29 16:42:10 -0500

    Update matToXML.py

--- matToXML.py
+++ matToXML.py
@@ -1,11 +1,5 @@
-import sys
-import os
-import warnings
-import random
-import cv2
-import numpy as np
-import torchvision
-import torchvision.transforms as T
-import torch
-import matplotlib.pyplot as plt
-from PIL import Image
+from scipy.io import loadmat
+
+# open .mat file
+annots = loadmat('../SVHN/test/digitStruct.mat')
+print(annots)
