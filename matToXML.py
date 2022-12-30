234ac07b2408693b4610351563b4bc6cdafc3f62
Roshan Kenia <keniaroshan@gmail.com>
2022-12-30 13:53:18 -0500

    Update matToXML.py

--- matToXML.py
+++ matToXML.py
@@ -6,5 +6,24 @@ bboxes = digitStruct['digitStruct']['bbox']
 names = digitStruct['digitStruct']['name']
 for i in range(len(bboxes)):
     print('Filename:', names[i], 'BBOX:', bboxes[i])
-np.save('SVHNbbox.npy', bboxes)
 np.save('SVHNname.npy', names)
+bboxesnormal = []
+for bbox in bboxes:
+    heights = bbox['height']
+    labels = bbox['label']
+    lefts = bbox['left']
+    top = bbox['top']
+    width = bbox['width']
+
+    digitData = []
+    for j in range(len(heights)):
+        xmin = lefts[i]
+        ymax = top[i]
+
+        xmax = xmin + width[i]
+        ymin = ymax - heights[i]
+
+        digitData.append((xmin, ymin, xmax, ymax, labels[i]))
+        print(digitData[i])
+    bboxesnormal.append(digitData)
+np.save('SVHNbbox.npy', bboxesnormal)
