5364506f8c6c72acb8f3d0a5ca6e67df499d94f1
Roshan Kenia <keniaroshan@gmail.com>
2022-12-30 13:27:07 -0500

    Update matToXML.py

--- matToXML.py
+++ matToXML.py
@@ -1,9 +1,4 @@
 # open .mat file
 import mat73
-import numpy as np
 digitStruct = mat73.loadmat('../SVHN/test/digitStruct.mat', use_attrdict=True)
-np.save('../SVHN/test/svhndict.npy', digitStruct)
-
-# Load
-digitStruct = np.load('svhndict.npy', allow_pickle='TRUE').item()
-print(digitStruct['name'])
+print(digitStruct['digitStruct'])
