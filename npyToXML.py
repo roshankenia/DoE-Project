21a6d5069045949b369801a34e509c25ee2f6d21
Roshan Kenia <keniaroshan@gmail.com>
2022-12-30 14:43:12 -0500

    Update npyToXML.py

--- npyToXML.py
+++ npyToXML.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 names = np.load('SVHNname.npy')
-bboxes = np.load('SVHNbbox.npy')
+bboxes = np.load('SVHNbbox.npy', allow_pickle=True)
 
 for i in range(len(names)):
     print('Filename:', names[i], "and BBOXes:", bboxes[i])
