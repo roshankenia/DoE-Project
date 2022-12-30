79e2e96f1f4f4aea0482b4b928cd7247a3a77de9
Roshan Kenia <keniaroshan@gmail.com>
2022-12-30 14:11:40 -0500

    Update matToXML.py

--- matToXML.py
+++ matToXML.py
@@ -9,22 +9,34 @@ for i in range(len(bboxes)):
 np.save('SVHNname.npy', names)
 bboxesnormal = []
 for bbox in bboxes:
-    heights = np.array(bbox['height'])
-    labels = np.array(bbox['label'])
-    lefts = np.array(bbox['left'])
+    # obtain bounding box information
+    height = np.array(bbox['height'])
+    label = np.array(bbox['label'])
+    left = np.array(bbox['left'])
     top = np.array(bbox['top'])
     width = np.array(bbox['width'])
-    print(type(heights))
-    print(heights.size)
+    print(type(height))
+    print(height.size)
     digitData = []
-    for j in range(heights.size):
-        xmin = lefts[i]
-        ymax = top[i]
+    if height.size == 1:
+        # only one bounding box
+        xmin = np.float64(left)
+        ymax = np.float64(top)
+        xmax = xmin + np.float64(width[i])
+        ymin = ymax - np.float64(height[i])
+        digitData.append((xmin, ymin, xmax, ymax, np.float64(label[i])))
+        print(digitData[0])
+    else:
+        # multiple bounding boxes found
+        for j in range(len(height)):
+            xmin = np.float64(left[i])
+            ymax = np.float64(top[i])
 
-        xmax = xmin + width[i]
-        ymin = ymax - heights[i]
+            xmax = xmin + np.float64(width[i])
+            ymin = ymax - np.float64(height[i])
 
-        digitData.append((xmin, ymin, xmax, ymax, labels[i]))
-        print(digitData[i])
+            digitData.append((xmin, ymin, xmax, ymax, np.float64(label[i])))
+            print(digitData[i])
+
     bboxesnormal.append(digitData)
 np.save('SVHNbbox.npy', bboxesnormal)
