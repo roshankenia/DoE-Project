17991caa4feeb746e80f46ede011ee5ed0e6494a
Roshan Kenia <keniaroshan@gmail.com>
2023-01-02 12:24:35 -0500

    Update pyTesseract_pred.py

--- pyTesseract_pred.py
+++ pyTesseract_pred.py
@@ -1,9 +1,9 @@
 from PIL import Image
-import pyTesseract
+import pytesseract
 import numpy as np
 
 filename = './ceramicimages/image301/0.jpg'
 img1 = np.array(Image.open(filename))
-text = pyTesseract.image_to_string(img1)
+text = pytesseract.image_to_string(img1)
 
 print(text)
