c6afa90f573d2f17595e06d14f0681ac3697815e
Roshan Kenia <keniaroshan@gmail.com>
2023-01-02 12:45:26 -0500

    Update pyTesseract_pred.py

--- pyTesseract_pred.py
+++ pyTesseract_pred.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import pytesseract
 import numpy as np
-pytesseract.pytesseract.tesseract_cmd = r'~/anaconda3/envs/tesseract2/bin/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'../../anaconda3/envs/tesseract2/bin/tesseract.exe'
 
 filename = './ceramicimages/image301/0.jpg'
 img1 = np.array(Image.open(filename))
