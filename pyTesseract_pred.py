3c922b5581b07fec4598bb9a69e2f28ad5c824e6
Roshan Kenia <keniaroshan@gmail.com>
2023-01-02 12:57:00 -0500

    Update pyTesseract_pred.py

--- pyTesseract_pred.py
+++ pyTesseract_pred.py
@@ -2,12 +2,17 @@ import os
 from PIL import Image
 import pytesseract
 import numpy as np
+import cv2
 pytesseract.pytesseract.tesseract_cmd = r'../../anaconda3/envs/tesseract2/bin/tesseract'
 tessdata_dir_config = r'../../anaconda3/envs/tesseract2/share/tessdata'
 os.environ["TESSDATA_PREFIX"] = tessdata_dir_config
 
 filename = './ceramicimages/image301/0.jpg'
-img1 = np.array(Image.open(filename))
-text = pytesseract.image_to_string(img1, config=tessdata_dir_config)
+img = np.array(Image.open(filename))
+norm_img = np.zeros((img.shape[0], img.shape[1]))
+img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
+img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)[1]
+img = cv2.GaussianBlur(img, (1, 1), 0)
+text = pytesseract.image_to_string(img, config=tessdata_dir_config)
 
 print('Result:', text)
