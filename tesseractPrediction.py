e632eb7748d1be616da48c7972ec90c84ff93963
Roshan Kenia <keniaroshan@gmail.com>
2023-01-05 12:23:46 -0500

    Update tesseractPrediction.py

--- tesseractPrediction.py
+++ tesseractPrediction.py
@@ -19,29 +19,29 @@ os.environ["TESSDATA_PREFIX"] = tessdata_dir_config
 # img = cv2.GaussianBlur(img, (1, 1), 0)
 # text = pytesseract.image_to_string(img, config=tessdata_dir_config)
 
-# imgNames = list(sorted(os.listdir("./trytess")))
-# for imgName in imgNames:
-#     img = cv2.imread(os.path.join("./trytess", imgName))
-#     print("File:", imgName)
-#     new_img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#     cv2.imwrite(os.path.join("./trytess", "og_"+imgName), new_img1)
-#     config = r'--oem 3 --psm 7 digits'
-#     text1 = pytesseract.image_to_string(new_img1, config=config)
-#     print('Result 1:', text1)
-#     new_img2 = cv2.resize(new_img1, None, fx=2.5, fy=2.5,
-#                           interpolation=cv2.INTER_CUBIC)
-#     cv2.imwrite(os.path.join("./trytess", "scale_"+imgName), new_img2)
-#     text2 = pytesseract.image_to_string(new_img2, config=config)
-#     print('Result 2:', text2)
-#     new_img1_ = cv2.cvtColor(new_img1, cv2.COLOR_BGR2GRAY)
-#     cv2.imwrite(os.path.join("./trytess", "gray_"+imgName), new_img1_)
-#     _, th1 = cv2.threshold(
-#         new_img1_, 175, 255, cv2.THRESH_BINARY)
-#     new_img3 = cv2.resize(th1, None, fx=2.5, fy=2.5,
-#                           interpolation=cv2.INTER_CUBIC)
-#     cv2.imwrite(os.path.join("./trytess", "bin_"+imgName), new_img3)
-#     text3 = pytesseract.image_to_string(new_img3, config=config)
-#     print('Result 3:', text3)
+imgNames = list(sorted(os.listdir("./trytess")))
+for imgName in imgNames:
+    img = cv2.imread(os.path.join("./trytess", imgName))
+    print("File:", imgName)
+    new_img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imwrite(os.path.join("./trytess", "og_"+imgName), new_img1)
+    config = r'--oem 3 --psm 7 digits'
+    text1 = pytesseract.image_to_string(new_img1, config=config)
+    print('Result 1:', text1)
+    new_img2 = cv2.resize(new_img1, None, fx=2.5, fy=2.5,
+                          interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite(os.path.join("./trytess", "scale_"+imgName), new_img2)
+    text2 = pytesseract.image_to_string(new_img2, config=config)
+    print('Result 2:', text2)
+    new_img1_ = cv2.cvtColor(new_img1, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(os.path.join("./trytess", "gray_"+imgName), new_img1_)
+    _, th1 = cv2.threshold(
+        new_img1_, 175, 255, cv2.THRESH_BINARY)
+    new_img3 = cv2.resize(th1, None, fx=2.5, fy=2.5,
+                          interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite(os.path.join("./trytess", "bin_"+imgName), new_img3)
+    text3 = pytesseract.image_to_string(new_img3, config=config)
+    print('Result 3:', text3)
 
 
 root = "./ceramicimages/"
@@ -51,6 +51,7 @@ if not os.path.isdir(pred_root):
 # need to iterate through each image folder
 imageFolders = list(sorted(os.listdir(root)))
 config = r'--oem 3 --psm 7 digits'
+clahe = cv2.createCLAHE(clipLimit=5, tileGridSize=(16, 16))
 for imageFolder in imageFolders:
     foldNum = ''.join(filter(lambda i: i.isdigit(), imageFolder))
     if foldNum == '300':
@@ -62,6 +63,7 @@ for imageFolder in imageFolders:
             rot = ''.join(filter(lambda i: i.isdigit(), rotation))
             print('Rotation:', rot)
             img = cv2.imread(os.path.join(root, imageFolder, rotation))
+            # original image
             new_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             # obtain prediction from tesseract
             text = pytesseract.image_to_string(new_img, config=config)
@@ -71,6 +73,38 @@ for imageFolder in imageFolders:
                 predDict[text] += 1
             else:
                 predDict[text] = 1
+
+            # resize image
+            new_img2 = cv2.resize(new_img, None, fx=2.5, fy=2.5,
+                                  interpolation=cv2.INTER_CUBIC)
+            text2 = pytesseract.image_to_string(new_img2, config=config)
+            print('Result 2:', text2)
+            if text2 in predDict:
+                predDict[text2] += 1
+            else:
+                predDict[text2] = 1
+
+            new_img1_ = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+            # apply CLAHE
+            img_clahe = clahe.apply(new_img1_)
+            text3 = pytesseract.image_to_string(img_clahe, config=config)
+            print('Result 3:', text3)
+            if text3 in predDict:
+                predDict[text3] += 1
+            else:
+                predDict[text3] = 1
+
+            # binarized image
+            _, th1 = cv2.threshold(
+                img_clahe, img_clahe.max()-40, 255, cv2.THRESH_BINARY)
+            new_img4 = cv2.resize(th1, None, fx=2.5, fy=2.5,
+                                  interpolation=cv2.INTER_CUBIC)
+            text4 = pytesseract.image_to_string(new_img4, config=config)
+            print('Result 4:', text4)
+            if text4 in predDict:
+                predDict[text4] += 1
+            else:
+                predDict[text4] = 1
         # write dictionary to file
         with open(os.path.join(pred_root, "preds_"+str(foldNum)+".txt"), 'w') as file:
             file.write(json.dumps(predDict))
