1a5882fe839c1dc109409c0a52991cab86d195f7
Roshan Kenia <keniaroshan@gmail.com>
2023-01-05 12:37:47 -0500

    Update tesseractPrediction.py

--- tesseractPrediction.py
+++ tesseractPrediction.py
@@ -74,37 +74,37 @@ for imageFolder in imageFolders:
         else:
             predDict[text] = 1
 
-        # resize image
-        new_img2 = cv2.resize(new_img, None, fx=2.5, fy=2.5,
-                              interpolation=cv2.INTER_CUBIC)
-        text2 = pytesseract.image_to_string(new_img2, config=config)
-        # print('Result 2:', text2)
-        if text2 in predDict:
-            predDict[text2] += 1
-        else:
-            predDict[text2] = 1
-
-        new_img1_ = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-        # # apply CLAHE
-        # img_clahe = clahe.apply(new_img1_)
-        # text3 = pytesseract.image_to_string(img_clahe, config=config)
-        # print('Result 3:', text3)
-        # if text3 in predDict:
-        #     predDict[text3] += 1
+        # # resize image
+        # new_img2 = cv2.resize(new_img, None, fx=2.5, fy=2.5,
+        #                       interpolation=cv2.INTER_CUBIC)
+        # text2 = pytesseract.image_to_string(new_img2, config=config)
+        # # print('Result 2:', text2)
+        # if text2 in predDict:
+        #     predDict[text2] += 1
         # else:
-        #     predDict[text3] = 1
+        #     predDict[text2] = 1
 
-        # binarized image
-        _, th1 = cv2.threshold(
-            new_img1_, new_img1_.max()-40, 255, cv2.THRESH_BINARY)
-        new_img4 = cv2.resize(th1, None, fx=2.5, fy=2.5,
-                              interpolation=cv2.INTER_CUBIC)
-        text4 = pytesseract.image_to_string(new_img4, config=config)
-        # print('Result 4:', text4)
-        if text4 in predDict:
-            predDict[text4] += 1
-        else:
-            predDict[text4] = 1
+        # new_img1_ = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+        # # # apply CLAHE
+        # # img_clahe = clahe.apply(new_img1_)
+        # # text3 = pytesseract.image_to_string(img_clahe, config=config)
+        # # print('Result 3:', text3)
+        # # if text3 in predDict:
+        # #     predDict[text3] += 1
+        # # else:
+        # #     predDict[text3] = 1
+
+        # # binarized image
+        # _, th1 = cv2.threshold(
+        #     new_img1_, new_img1_.max()-40, 255, cv2.THRESH_BINARY)
+        # new_img4 = cv2.resize(th1, None, fx=2.5, fy=2.5,
+        #                       interpolation=cv2.INTER_CUBIC)
+        # text4 = pytesseract.image_to_string(new_img4, config=config)
+        # # print('Result 4:', text4)
+        # if text4 in predDict:
+        #     predDict[text4] += 1
+        # else:
+        #     predDict[text4] = 1
     # write dictionary to file
     with open(os.path.join(pred_root, "preds_"+str(foldNum)+".txt"), 'w') as file:
         file.write(json.dumps(predDict))
