4ad143899657c1769e5802608ecb10b8dad4330b
Roshan Kenia <keniaroshan@gmail.com>
2023-01-05 12:25:21 -0500

    Update tesseractPrediction.py

--- tesseractPrediction.py
+++ tesseractPrediction.py
@@ -19,29 +19,29 @@ os.environ["TESSDATA_PREFIX"] = tessdata_dir_config
 # img = cv2.GaussianBlur(img, (1, 1), 0)
 # text = pytesseract.image_to_string(img, config=tessdata_dir_config)
 
-imgNames = list(sorted(os.listdir("./trytess")))
-for imgName in imgNames:
-    img = cv2.imread(os.path.join("./trytess", imgName))
-    print("File:", imgName)
-    new_img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    cv2.imwrite(os.path.join("./trytess", "og_"+imgName), new_img1)
-    config = r'--oem 3 --psm 7 digits'
-    text1 = pytesseract.image_to_string(new_img1, config=config)
-    print('Result 1:', text1)
-    new_img2 = cv2.resize(new_img1, None, fx=2.5, fy=2.5,
-                          interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite(os.path.join("./trytess", "scale_"+imgName), new_img2)
-    text2 = pytesseract.image_to_string(new_img2, config=config)
-    print('Result 2:', text2)
-    new_img1_ = cv2.cvtColor(new_img1, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(os.path.join("./trytess", "gray_"+imgName), new_img1_)
-    _, th1 = cv2.threshold(
-        new_img1_, 175, 255, cv2.THRESH_BINARY)
-    new_img3 = cv2.resize(th1, None, fx=2.5, fy=2.5,
-                          interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite(os.path.join("./trytess", "bin_"+imgName), new_img3)
-    text3 = pytesseract.image_to_string(new_img3, config=config)
-    print('Result 3:', text3)
+# imgNames = list(sorted(os.listdir("./trytess")))
+# for imgName in imgNames:
+#     img = cv2.imread(os.path.join("./trytess", imgName))
+#     print("File:", imgName)
+#     new_img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     cv2.imwrite(os.path.join("./trytess", "og_"+imgName), new_img1)
+#     config = r'--oem 3 --psm 7 digits'
+#     text1 = pytesseract.image_to_string(new_img1, config=config)
+#     print('Result 1:', text1)
+#     new_img2 = cv2.resize(new_img1, None, fx=2.5, fy=2.5,
+#                           interpolation=cv2.INTER_CUBIC)
+#     cv2.imwrite(os.path.join("./trytess", "scale_"+imgName), new_img2)
+#     text2 = pytesseract.image_to_string(new_img2, config=config)
+#     print('Result 2:', text2)
+#     new_img1_ = cv2.cvtColor(new_img1, cv2.COLOR_BGR2GRAY)
+#     cv2.imwrite(os.path.join("./trytess", "gray_"+imgName), new_img1_)
+#     _, th1 = cv2.threshold(
+#         new_img1_, 175, 255, cv2.THRESH_BINARY)
+#     new_img3 = cv2.resize(th1, None, fx=2.5, fy=2.5,
+#                           interpolation=cv2.INTER_CUBIC)
+#     cv2.imwrite(os.path.join("./trytess", "bin_"+imgName), new_img3)
+#     text3 = pytesseract.image_to_string(new_img3, config=config)
+#     print('Result 3:', text3)
 
 
 root = "./ceramicimages/"
